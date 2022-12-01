87f1244cac932fab05c4e37bff265df381ad6278
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-01 13:59:02 +0100

    update with constructor method

--- src/it/develhope/Ex2/Competition.java
+++ src/it/develhope/Ex2/Competition.java
@@ -13,26 +13,30 @@ public class Competition {
         teamB.teamName="TeamNumber2";
 
         //WHY I CAN'T MAKE THINGS EASIER WITH THIS?
-        /*teamA.p1.Programmer("Gaetano","Java",1);
+        teamA.p1.Programmer("Gaetano","Java",1);
         teamA.p2.Programmer("Franco","python",1);
 
         teamB.p1.Programmer("Ciccio","Java",12);
-        teamB.p2.Programmer("Lello","Python",4);*/
+        teamB.p2.Programmer("Lello","Python",4);
 
         //PROGRAMMERS
-        teamA.p1.name="Francesco";
+        //TEAM A
+        /*teamA.p1.name="Francesco";
         teamA.p1.programmingLanguage="Java";
         teamA.p1.yearsOfExperience=3;
+
         teamA.p2.name="Giacomo";
         teamA.p2.programmingLanguage="Python";
         teamA.p2.yearsOfExperience=1;
-
+        //TEAM B
         teamB.p1.name="Leonardo";
         teamB.p1.programmingLanguage="Java";
         teamB.p1.yearsOfExperience=8;
+
         teamB.p2.name="Pietro";
         teamB.p2.programmingLanguage="C++";
-        teamB.p2.yearsOfExperience=4;
+        teamB.p2.yearsOfExperience=4;*/
+
         //PRINT ALL VALUES
         teamA.printTeamDetails();
         teamB.printTeamDetails();
--- src/it/develhope/Ex2/Programmer.java
+++ src/it/develhope/Ex2/Programmer.java
@@ -6,9 +6,12 @@ public class Programmer {
     public int yearsOfExperience;
 
     //WHY I CAN'T MAKE THINGS EASIER WITH THIS?
-    /*public String Programmer(String name,String programmingLanguage, int yearsOfExperience){
-        return name+" "+programmingLanguage+" "+yearsOfExperience;
-    }*/
+    public String Programmer(String name,String programmingLanguage, int yearsOfExperience){
+        this.name=name;
+        this.programmingLanguage=programmingLanguage;
+        this.yearsOfExperience=yearsOfExperience;
+        return name+programmingLanguage+yearsOfExperience;
+    }
 
     public void printProgrammerDetails(String name,String programmingLanguage,int yearsOfExperience){
         System.out.println(name+ " " +programmingLanguage+" "+yearsOfExperience);
